cfa719ecb8613dd36e138299651bf8478f1701e7
Debendra Dash <141917659+Debendradash2003@users.noreply.github.com>
2024-10-25 14:29:31 +0000

    saved

--- b.js
+++ b.js
@@ -1,1 +1,66 @@
-console.log("hello")
+// let score =33
+// let age ="23abs"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let chang = Number(age )
+// console.log(typeof (age));
+// console.log(age);
+
+// let add= null
+
+// console.log(add);
+
+
+
+// let value =true
+// let value1 =false
+// console.log(Number (value1));
+
+// console.log(Number (value));
+
+let some =123
+let x= String(some)
+console.log(some);
+
+console.log(typeof x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ope.js
@@ -0,0 +1,27 @@
+// let value =4
+// //console.log(-value);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+
+// let str ="debendra"
+// let str1 ="dash"
+// console.log(str + str1);
+
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(1+"2"+2);
+// console.log("1"+2);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
