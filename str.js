5dc35262bb018407aff02c77ab24040699039716
Debendra Dash <141917659+Debendradash2003@users.noreply.github.com>
2024-10-25 19:08:48 +0000

    next day continue

---
+++ str.js
@@ -0,0 +1,60 @@
+// const name = "Debendra"
+// const count =20
+
+
+// console.log(name +count +"lunch");
+
+// console.log(`my name is  ${name} and count lunch is  ${count}`);
+
+
+// const gamename = new String ('Debendra ')
+// console.log(gamename [0]);
+
+// console.log(gamename.__proto__);
+
+// console.log(gamename.length);
+
+// console.log(gamename.charAt(3));
+// console.log(gamename .indexOf('d'));
+
+
+
+// let name = "Debendra "
+
+// let newname = name.substring(0,10)
+
+// console.log(newname);
+
+// let another =name .slice(-8,0)
+// console.log(another);
+
+
+// let chat = "           khana khalena tum            "
+
+// console.log(chat);
+// console.log(chat.trim());
+
+const url = "www.youtub.com%2003wxllnb"
+
+console.log(url.replace("%2003","__"));
+
+console.log(url.includes("youtub"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
